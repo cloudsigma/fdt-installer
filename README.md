5bf1a0c41ac6a32de97b57acca40cae89c4b8580
Viktor Petersson <vpetersson@wireload.net>
2014-02-19 15:59:45 -0800

    First\!

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-fdt-installer
-=============
+# Fast Data Transfer (FDT) installer.
 
-Fast Data Transfer (FDT) installer.
+A simple tool to streamline the installation of [Fast Data Transfer](http://monalisa.cern.ch/FDT/).
+
+Warning: Only tested on CentOS 6.4.
+
+Usage:
+
+    curl -sL https://raw.github.com/cloudsigma/fdt-installer/master/install.sh | bash
+
---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Installation script for Fast Data Transfer (FDT)
+# from http://monalisa.cern.ch/FDT/
+
+FDT='/usr/bin/fdt.jar'
+
+# Make sure we're running as root
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root."
+  exit 1
+fi
+
+echo "Installing Java (OpenJDK)..."
+yum install -y -q java-1.7.0-openjdk > /dev/null
+
+echo "Fetching Fast Data Transfer (FDT)..."
+curl -sl -o $FDT http://monalisa.cern.ch/FDT/lib/fdt.jar
+
+echo "Starting FDT.."
+java -jar $FDT > /var/log/fdt.log 2>&1 &
+
+echo -e "To start FDT again, please run:\n$ java -jar $FDT > /var/log/fdt.log 2>&1\nTo kill FDT, run:\n$ pkill -f fdt.jar"
+echo "Make sure to adjust your firewall accordingly."
